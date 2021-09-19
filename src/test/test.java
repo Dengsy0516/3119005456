9d137d08cf6782dbbd3c1b392970512baeffb42b
邓Frank <Dengsy0516@163.com>
2021-09-19 18:08:17 +0800

    新增加了4个测试样例

--- src/test/test.java
+++ src/test/test.java
@@ -54,4 +54,68 @@ public class test {
             e.printStackTrace();
         }
     }
+
+    @Test
+    public void dis1File(){
+        try{
+            application.process("E:\\ideaproject\\src\\main\\resources\\orig.txt","E:\\ideaproject\\src\\main\\resources\\orig_0.8_dis_1.txt");
+        }catch (IOException e){
+            e.printStackTrace();
+        }
+    }
+
+    @Test
+    public void dis7File(){
+        try{
+            application.process("E:\\ideaproject\\src\\main\\resources\\orig.txt","E:\\ideaproject\\src\\main\\resources\\orig_0.8_dis_7.txt");
+        }catch (IOException e){
+            e.printStackTrace();
+        }
+    }
+
+    @Test
+    public void dis10File(){
+        try{
+            application.process("E:\\ideaproject\\src\\main\\resources\\orig.txt","E:\\ideaproject\\src\\main\\resources\\orig_0.8_dis_10.txt");
+        }catch (IOException e){
+            e.printStackTrace();
+        }
+    }
+
+    @Test
+    public void dis15File(){
+        try{
+            application.process("E:\\ideaproject\\src\\main\\resources\\orig.txt","E:\\ideaproject\\src\\main\\resources\\orig_0.8_dis_15.txt");
+        }catch (IOException e){
+            e.printStackTrace();
+        }
+    }
+
+    @Test
+    public void forMixFile(){
+        try{
+            application.process("E:\\ideaproject\\src\\main\\resources\\orig.txt","E:\\ideaproject\\src\\main\\resources\\orig_0.8_mix.txt");
+        }catch (IOException e){
+            e.printStackTrace();
+        }
+    }
+
+    @Test
+    public void forSub1File(){
+        try{
+            application.process("E:\\ideaproject\\src\\main\\resources\\orig.txt","E:\\ideaproject\\src\\main\\resources\\orig_sub_1.txt");
+        }catch (IOException e){
+            e.printStackTrace();
+        }
+    }
+
+
+    @Test
+    public void forSub2File(){
+        try{
+            application.process("E:\\ideaproject\\src\\main\\resources\\orig.txt","E:\\ideaproject\\src\\main\\resources\\orig_sub_2.txt");
+        }catch (IOException e){
+            e.printStackTrace();
+        }
+    }
 }
